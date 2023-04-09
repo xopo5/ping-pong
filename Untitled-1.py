ab0a45f890d38012c825b53442a7f5b454a7951b
xopo5 <128968445+xopo5@users.noreply.github.com>
2023-04-09 14:31:33 +0500

    Update Untitled-1.py

--- Untitled-1.py
+++ Untitled-1.py
@@ -7,22 +7,37 @@ class GameSprite(sprite.Sprite):
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
-        def reset(self):
-            mw.blit(self.image, (self.rect.x, self.rect.y))
+    
+    def reset(self):
+        mw.blit(self.image, (self.rect.x, self.rect.y))
 
-BG_COLOR = (220, 100, 220)
-WIDTH, HEIGHT = 600, 480
+class Player(GameSprite):
+    def __init__(self, sprite_image, x=0, y=0, width=50, height=50, speed=5, key_up=K_w, key_down=K_s):
+        super().__init__(sprite_image, x, y, width, height)
+        self.speed = speed
+        self.key_down = key_down
+        self.key_up = key_up
+    def update(self):
+        keys = key.get_pressed()
+        if keys[self.key_up]:
+            self.rect.y -= self.speed
+        if keys[self.key_down]:
+            self.rect.y += self.speed
+BG_COLOR = (220,100,220)
+WIDTH,HEIGHT = 600, 480
 FPS = 60
 
 mw = display.set_mode((WIDTH, HEIGHT))
-display.set_caption("Ping Pong")
+display.set_caption('Пинг-Понг')
 mw.fill(BG_COLOR)
 clock = time.Clock()
-run = True
 
+run = True
 while run:
+
     for e in event.get():
         if e.type == QUIT:
             run = False
+    
     display.update()
     clock.tick(FPS)
